78925ac77aae5e1412e91e3ce899b80865e9a6be
ANEM <anembirelater@gmail.com>
2026-06-10 00:39:15 +0100

    Change server port from 3000 to 10000

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(express.json());
 
 let command = "none";
 
-// مسار للتحقق الأساسي
 app.get('/', (req, res) => {
     res.send("Server is running!");
 });
 
-// مسار التحقق من الأوامر
 app.get('/check', (req, res) => {
     res.json({ status: "success", cmd: command });
     command = "none";
 });
 
-// مسار إرسال الأوامر
 app.post('/push', (req, res) => {
     command = req.body.cmd;
     res.send("Command received: " + command);
